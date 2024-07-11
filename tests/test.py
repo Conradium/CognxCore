bc9d77b575814204135f048cdabc44ab61e9bb82
Conradium <40384712+Conradium@users.noreply.github.com>
2024-07-11 23:15:30 +0900

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
-from cogni import CogniCore as cc
+from cognxcore import CogniCore
 
 
 api_key = input("Enter your API key: ")
-
-ai = cc(api_key)
+ai = CogniCore(api_key)
 
 prompt = input("Enter your prompt: ")
 response = ai.generate_content(prompt)
